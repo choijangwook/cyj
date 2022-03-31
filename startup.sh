2b3c97b0400f02850f6d769b591743d0c392b396
choi jang wook <jangwookchoi1@gmail.com>
2022-03-31 10:49:35 +0900

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env bash
+set -x
+set -e
+
+echo "Downloading ${SERVICE_JAR_URL}"
+curl -L ${SERVICE_JAR_URL} > /tmp/application.jar
+
+java -jar /tmp/application.jar ${SERVICE_JAVA_ARGUMENTS}
